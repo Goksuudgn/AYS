16fa7f71bc4002c125ec5d2601a0b4e67de97d48
Goksuudgn <goksuudogan@gmail.com>
2024-08-25 22:07:37 +0300

    mig ve update yapıldı

---
+++ AYS.Data/Migrations/20240825185804_ModelsUpdateAndRepository.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AYS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelsUpdateAndRepository : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "RoomId",
+                table: "Rooms",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "DeviceId",
+                table: "Devices",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "DevicePhotoId",
+                table: "DevicePhotos",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Categories",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Rooms",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Homes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Devices",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "DevicePhotos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Homes");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "DevicePhotos");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Categories");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Rooms",
+                newName: "RoomId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Devices",
+                newName: "DeviceId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "DevicePhotos",
+                newName: "DevicePhotoId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "CategoryId");
+        }
+    }
+}
