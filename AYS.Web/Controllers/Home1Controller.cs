3daf274e780dd08b8a392bccea97e30ede24d055
Goksuudgn <goksuudogan@gmail.com>
2024-09-02 18:39:42 +0300

    Home1 Controller yapıldı

---
+++ AYS.Web/Controllers/Home1Controller.cs
@@ -0,0 +1,48 @@
+﻿using AYS.Data;
+using AYS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AYS.Web.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Home1Controller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+            var result = _context.Homes.ToList();
+            return Json(new { data = result });
+        }
+        public IActionResult Add(Home home)
+        {
+            _context.Homes.Add(home);
+            _context.SaveChanges();
+            return Ok(home);
+        }
+        public IActionResult Update(Home home)
+        {
+           _context.Homes.Update(home);
+            _context.SaveChanges();
+            return Ok(home);
+        }
+        public IActionResult Delete(int id)
+        {
+                //SOFT DELETE
+                Home home = _context.Homes.Find(id);
+                home.IsDeleted = true;
+                _context.Homes.Update(home);
+                _context.SaveChanges();
+                return Ok(home);
+        }
+
+    }
+}
